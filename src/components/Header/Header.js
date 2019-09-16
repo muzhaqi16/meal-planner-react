a34736f1132c0a15d44ec4b1661768633773fe82
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-09-16 19:58:25 -0400

    add navigation header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import './Header.css'
 
 export default class Header extends React.Component {
     render() {
-        return (<></>)
+        return (
+            <header>
+                <div id="logo">
+                    <h2><a href="index.html">Meal Planner</a></h2>
+                </div>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="signin">Log in</Link>
+                    <Link to="planner">Plan It</Link>
+                </nav>
+            </header>)
     }
 }
