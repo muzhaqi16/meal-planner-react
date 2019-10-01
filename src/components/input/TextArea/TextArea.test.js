de30b8a394d56d758afda605262fa02d30190f22
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-10-01 01:51:57 -0400

    make sure all tests are implemented

--- src/components/input/TextArea/TextArea.test.js
+++ src/components/input/TextArea/TextArea.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import TextInput from './TextInput';
-describe('TextInput Component', () => {
+import TextArea from './TextArea';
+describe('TextArea Component', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<TextInput />, div);
+        ReactDOM.render(<TextArea />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
     it('renders the UI as expected', () => {
         const input = renderer
-            .create(<TextInput />)
+            .create(<TextArea />)
             .toJSON();
         expect(input).toMatchSnapshot();
     });
